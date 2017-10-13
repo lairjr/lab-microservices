202855cfc78f9052d550f23b230af8382b85f00a
lairjr <lairjr@outlook.com>
2017-10-13 16:06:16 -0300

    Add programs endpoint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 var express = require('express');
+var JSONAPISerializer = require('jsonapi-serializer').Serializer;
+
 var app = express();
+var ProgramSerializer = new JSONAPISerializer('programs', {
+  attributes: ['title', 'category']
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
-})
+});
+
+app.get('/programs', function (req, res) {
+  var programData = [
+    {
+      id: 1,
+      title: 'Jornal do Almoço',
+      category: 'Notícias'
+    }
+  ];
+  res.send(ProgramSerializer.serialize(programData));
+});
 
 app.listen(3000);
